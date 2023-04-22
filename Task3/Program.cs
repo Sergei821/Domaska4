74e1fc9a695c33411761447cc3168a677c757a59
Sergei821 <ps16111982@gmail.com>
2023-04-22 22:56:06 +0300

    zad3

---
+++ Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+int InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillingTheArray(int arrLength)
+{
+    int[] arr = new int[arrLength];
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = InputNumberWithMessage($"Введите {i + 1} - й элемент массива: ");
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == (arr.Length - 1))
+        {
+            Console.Write($"{arr[i]};");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    }
+}
+
+int arrLength = InputNumberWithMessage("Введите длину массива: ");
+int[] arr = FillingTheArray(arrLength);
+PrintArray(arr);
+
