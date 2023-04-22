b9450d9bfd4312bc3647fc72cc390a4b1be3e089
Sergei821 <ps16111982@gmail.com>
2023-04-22 22:41:40 +0300

    zad1

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+int InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Exponentiation(int number, int degree)
+{
+    int result = number;
+    if (degree > 0)
+    {
+      for (int i = 2; i <= degree; i++)
+      {
+            result = result * number; 
+      }
+    }
+    else
+    {
+        System.Console.WriteLine("неправильно");
+    }
+    Console.WriteLine("Число A в степени B = " + result);
+}
+int number = InputNumberWithMessage("Введите число A: ");
+int degree = InputNumberWithMessage("Введите число B: ");
+
+Exponentiation(number, degree);
+
+
